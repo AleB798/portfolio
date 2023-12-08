525840b15090c86009f07b6a35d9bda55d32a353
Alexandra Baumann <alexbaumann342@gmail.com>
2023-12-08 11:09:40 +0100

    test routage basename

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './styles/global.scss';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { createRoot } from "react-dom/client";
+
+//page
 import Home from './pages/Home';
 
 const root = createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route path="/" element={<Home />} />
             </Routes>
